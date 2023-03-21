3fb76d4c1b73e113c10acefea94adb85c9f19768
polina <opilane@HP-CZC2349HTP>
2023-03-21 15:35:27 +0200

    .

---
+++ oval.py
@@ -0,0 +1,32 @@
+﻿import tkinter as tk
+from tkinter import*
+from tkinter import font
+from random import*
+colors = ["black",
+           "cyan",
+           "magenta",
+           "red",
+           "blue",
+           "grey",
+           "yellow",
+           "green",
+           "lightblue",
+           "pink",
+           "gold"]
+raam2 = Tk
+raam2.title("Ringid")
+tahvel2 = Canvas(raam2, width=600, height=600, background="while")
+x0=0
+y0=0
+x1=600
+y1=600
+p=5
+for i in range(55):
+    x0+=p
+    y0+=p
+    x1-=p
+    y1-=p
+    tahvel2.create_oval(x0,y0,x1,y1, fill=choice(colors))
+    #sleep(1)
+tahvel2.gird()
+raam2.mainloop()
