2eb3598164efc87b022f9854f7ee065ba590a9d3
opilane <opilane@HP-CZC2349HTP>
2023-03-28 13:24:27 +0300

    ..

--- Muster.py
+++ Muster.py
@@ -1,20 +1,2 @@
 ﻿from tkinter import*
 
-def valik5():
-        raam = Tk()
-        raam.title("Tahvel")
-        tahvel = Canvas(raam, width=350, height=350, background="white")
-        tahvel.grid()
-
-        x1=35
-        y1=x1*8
-        for x in range(8):
-            for c in range (8):
-                x2=x*x1
-                y2=c*x1
-                if(x+c)%2==0:
-                    color="white"
-                else:
-                    color="black"
-                y=tahvel.create_rectangle(x2,y2,x2+x1,y2+x1,fill=color)
-
--- VALGUSFOOR.py
+++ VALGUSFOOR.py
@@ -1,18 +1,16 @@
 ﻿import tkinter as tk
 from tkinter import*
 from tkinter import font
+from math import*
 raam = Tk()
 raam.title("Tahvel")
-tahvel = Canvas(raam, width=1000, height=1000, background="white")
-tahvel.create_line(320, 550,570,550, width=500 , fill="#a3a7ad")
-suur_font = font.Font (family='Helvetica', size=20, weight='bold')
-tahvel.create_text (370, 310, text="Valgusfoor", font=suur_font, anchor=NW) 
-tahvel.create_line(420, 400,470,400, width=45, fill="red")
-tahvel.create_line(420, 450,470,450, width=45, fill="yellow")
-tahvel.create_line(420, 500,470,500, width=45, fill="green")
-
-tahvel.create_line(445,530, 445,750, width=8, fill="black")
-tahvel.create_line(350,760, 530,760,width=4, fill="black")
-
-
+tahvel = Canvas(raam, width=650, height=800, background="white")
+tahvel.grid()
+tahvel.create_line(320,550,  570,550,  width=500 , fill="#5c5959")
+suur_font = font.Font(family='Helvetica', size=20, weight='bold')
+tahvel.create_line(420,400,  470,400,  width=45 , fill="red")
+tahvel.create_line(420,450,  470,450,  width=45 , fill="yellow")
+tahvel.create_line(420,500,  470,500,  width=45 , fill="green")
+tahvel.create_line(445 ,530,  445,750,  width=8 , fill="black")
+tahvel.create_line(350,760,  530,760,  width=4 , fill="black")
 raam.mainloop()
